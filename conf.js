742c28239b4a7fda7623690605ef74740dabf779
kinglyl <954452738@qq.com>
2019-07-31 21:53:16 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -85,8 +85,8 @@ jsproxy_config({
   /**
    * 默认节点
    */
-   node_default: 'mycfworker',
-  //node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
+   //node_default: 'mycfworker',
+  node_default: /52proxy\.\w+$/.test(location.host) ? 'mysite' : 'mycfworker',
 
   /**
    * 加速节点
