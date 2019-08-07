ecdf25cefe984378afeda7890329bca5a21f82aa
kinglyl <36796881+kinglyl@users.noreply.github.com>
2019-08-07 12:38:51 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -11,22 +11,7 @@ jsproxy_config({
   },
 
   // 节点配置
-  node_map: {/*
-    'demo-hk': {
-      label: '演示服务-香港节点',
-      lines: {
-        // 主机:权重
-        'node-aliyun-hk-0.etherdream.com:8443': 1,
-        'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 1,
-      }
-    },
-    'demo-sg': {
-      label: '演示服务-新加坡节点',
-      lines: {
-        'node-aliyun-sg.etherdream.com:8443': 1,
-      },
-    },*/
+  node_map: {
 	'mycfworker': {
 		label: 'Cloudflare加速节点-推荐',
 		lines: {
@@ -97,10 +82,10 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  //assets_cdn: 'https://cdn.jsdelivr.net/gh/52proxy/52proxy.github.io@master/assets/',
+  assets_cdn: 'https://cdn.jsdelivr.net/gh/52proxy/52proxy.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
-   assets_cdn: 'assets/',
+   //assets_cdn: 'assets/',
 
   // 首页路径
   index_path: 'index_v3.html',
